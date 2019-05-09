ca43921c6fb5872e2b7eed03aa5b57f1cfe0540e
H.Park <hyeongminpark5@gmail.com>
2019-05-09 16:38:40 +0900

    save folder debug

--- implementations/wgan_gp/wgan_gp.py
+++ implementations/wgan_gp/wgan_gp.py
@@ -16,8 +16,6 @@ import torch.nn.functional as F
 import torch.autograd as autograd
 import torch
 
-os.makedirs("images", exist_ok=True)
-
 parser = argparse.ArgumentParser()
 parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
@@ -35,6 +33,8 @@ parser.add_argument("--sample_interval", type=int, default=400, help="interval b
 parser.add_argument("--save_folder", type=str, default="images", help="generated sample folder")
 opt = parser.parse_args()
 print(opt)
+
+os.makedirs(opt.save_folder, exist_ok=True)
 
 img_shape = (opt.channels, opt.img_size, opt.img_size)
 
